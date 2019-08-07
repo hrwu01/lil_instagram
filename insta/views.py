97540da103557afbdbad583bf504c07cf4c71fec
hrwu01 <hrwu01@gmail.com>
2019-08-07 08:57:56 +0800

    LoginRequiredMixin

--- insta/views.py
+++ insta/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from insta.models import Post
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class InstaRoot(TemplateView):
@@ -16,21 +17,22 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     template_name = 'create_post.html'
     fields = '__all__'
     success_url = reverse_lazy('posts')
+    login_url = 'login'
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin,UpdateView):
     model = Post
     template_name = 'update_post.html'
     fields = '__all__'
     success_url = reverse_lazy('posts')
+    login_url = 'login'
     
-    
-
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin,DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('posts')
+    login_url = 'login'
