21cf7f523e3609a03b27ce5c94420aadd8b7f222
hrwu01 <hrwu01@gmail.com>
2019-08-14 14:08:13 +0800

    before follow/unfollow

--- insta/views.py
+++ insta/views.py
@@ -13,15 +13,15 @@ class PostListView(ListView):
     model = Post
     template_name = 'posts.html'
 
-    def get_queryset(self):
-        if not self.request.user.is_authenticated:
-            return None
-        current_user = self.request.user
-        following = set()
-        following.add(current_user)
-        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
-            following.add(conn.following)
-        return Post.objects.filter(author__in=following)
+    # def get_queryset(self):
+    #     if not self.request.user.is_authenticated:
+    #         return None
+    #     current_user = self.request.user
+    #     following = set()
+    #     following.add(current_user)
+    #     for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+    #         following.add(conn.following)
+    #     return Post.objects.filter(author__in=following)
 
 class PostDetailView(DetailView):
     model = Post
