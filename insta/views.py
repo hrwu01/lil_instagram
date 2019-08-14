b54af4e68b07ec829f294ea7261688af708c3351
hrwu01 <hrwu01@gmail.com>
2019-08-14 14:15:52 +0800

    require login for detail views

--- insta/views.py
+++ insta/views.py
@@ -23,9 +23,10 @@ class PostListView(ListView):
     #         following.add(conn.following)
     #     return Post.objects.filter(author__in=following)
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin,DetailView):
     model = Post
     template_name = 'post_detail.html'
+    login_url = 'login'
 
 class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
